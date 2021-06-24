47fc7ec0d9c83952ad9ceb08e19e25e437cb1485
DaanV2 <daanverstraten@hotmail.com>
2021-06-24 13:32:08 +0200

    Updated

--- src/Lib/Project/BehaviorPack/Types/Entity/Process.ts
+++ src/Lib/Project/BehaviorPack/Types/Entity/Process.ts
@@ -1,6 +1,7 @@
 import * as internal from "../../../../Internal/BehaviorPack/Entity";
 import { Json } from "../../../../Internal/Json";
 import { MolangSet } from "../../../../Molang/MolangSet";
+import { DefinedUsing } from "../../../../Types/Defined Using/DefinedUsing";
 import { Location } from "../../../../Types/Location/Location";
 import { Map } from "../../../../Types/Map/Map";
 import { TextDocument } from "../../../../Types/TextDocument/TextDocument";
@@ -22,7 +23,24 @@ export function Process(doc: TextDocument): Entity | undefined {
   const container = imp["minecraf:entity"];
   const id = container.description.identifier;
 
-  //TODO
+  const out: Entity = {
+    id: id,
+    location: Location.create(uri, content.indexOf(id)),
+    documentation: `BP Entity: ${id}`,
+    animations: DefinedUsing.empty(),
+    events: [],
+    groups: [],
+    molang: MolangSet.harvest(container),
+  };
 
-  return undefined;
+  if (container.component_groups) Map.forEach(container.component_groups, (group, name) => out.groups.push(name));
+  if (container.events) Map.forEach(container.events, (event, name) => out.events.push(name));
+  if (container.description.animations) {
+    Map.forEach(container.description.animations, (anim, name) => {
+      out.animations.defined.push(name);
+      out.animations.using.push(anim);
+    });
+  }
+
+  return out;
 }
