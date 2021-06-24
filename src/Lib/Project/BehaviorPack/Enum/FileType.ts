2333cd16645555b6e88e690f7d76935fd3ba38a8
DaanV2 <daanverstraten@hotmail.com>
2021-06-24 12:10:02 +0200

    Fixing

--- src/Lib/Project/BehaviorPack/Enum/FileType.ts
+++ src/Lib/Project/BehaviorPack/Enum/FileType.ts
@@ -1,5 +1,5 @@
 /***/
-export enum FileType {  
+export enum FileType {
   /***/
   animation,
   /***/
@@ -30,26 +30,25 @@ export enum FileType {
 
 export namespace FileType {
   /**Detects behavior pack file type, already assumed the path belongs to a behavior pack
-  * @param uri The filepath to examine, expects slashes to be '/'*/
+   * @param uri The filepath to examine, expects slashes to be '/'*/
   export function detect(uri: string): FileType {
-      if (uri.endsWith(".mcfunction")) return FileType.function;
+    if (uri.endsWith(".mcfunction")) return FileType.function;
 
-      //Folders
-      if (uri.includes("/animation_controllers/")) return FileType.animation_controller;
-      if (uri.includes("/animations/")) return FileType.animation;
-      if (uri.includes("/blocks/")) return FileType.block;
-      if (uri.includes("/entities/")) return FileType.entity;
-      if (uri.includes("/functions/")) return FileType.function;
-      if (uri.includes("/items/")) return FileType.item;
-      if (uri.includes("/loot_tables/")) return FileType.loot_table;
-      if (uri.includes("/scripts/")) return FileType.script;
-      if (uri.includes("/spawn_rules/")) return FileType.spawn_rule;
-      if (uri.includes("/trading/")) return FileType.trading;
+    //Folders
+    if (uri.includes("/animation_controllers/")) return FileType.animation_controller;
+    if (uri.includes("/animations/")) return FileType.animation;
+    if (uri.includes("/blocks/")) return FileType.block;
+    if (uri.includes("/entities/")) return FileType.entity;
+    if (uri.includes("/functions/")) return FileType.function;
+    if (uri.includes("/items/")) return FileType.item;
+    if (uri.includes("/loot_tables/")) return FileType.loot_table;
+    if (uri.includes("/scripts/")) return FileType.script;
+    if (uri.includes("/spawn_rules/")) return FileType.spawn_rule;
+    if (uri.includes("/trading/")) return FileType.trading;
 
-      //Files
-      if (uri.includes("manifest.json")) return FileType.manifest;
+    //Files
+    if (uri.includes("manifest.json")) return FileType.manifest;
 
-      return FileType.unknown;
-    }
+    return FileType.unknown;
   }
 }
