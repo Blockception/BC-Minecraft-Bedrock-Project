f25cb64f9826fe2a38cee296c0a1daef019eda51
DaanV2 <daanverstraten@hotmail.com>
2021-06-25 16:07:36 +0200

    Redoing exports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,29 @@
-export * from "./Lib/Internal/include";
-export * from "./Lib/Molang/include";
-export * from "./Lib/Project/include";
-export * from "./Lib/Types/include";
+export { Json, Types } from "./Lib/Internal/include";
+export { DataContent, Molang, MolangFullSet, MolangSet, RegularExpression } from "./Lib/Molang/include";
+export { BehaviorPack, PackType, ProjectData, ResourcePack } from "./Lib/Project/include";
+export {
+  Defined,
+  DefinedUsing,
+  Documentated,
+  JsonPath,
+  Map,
+  Range,
+  Container,
+  DataSet,
+  DataSetConnected,
+  DataSetSingle,
+  DataSetUnconnected,
+  Documentation,
+  FileReference,
+  Identifiable,
+  JsonFileReference,
+  JsonFileSummarizedReference,
+  Locatable,
+  Location,
+  Pack,
+  Position,
+  Script,
+  TextDocument,
+  Using,
+  VanillaConnector,
+} from "./Lib/Types/include";
