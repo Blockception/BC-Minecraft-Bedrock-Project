64d7a477562fd507d8ac596c93d2bf65050b3df0
DaanV2 <daanverstraten@hotmail.com>
2021-08-14 18:30:26 +0200

    Fixed import

--- test/Molang/Queries.test.ts
+++ test/Molang/Queries.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
-import { Molang } from "../../../src/Lib/Molang/Molang";
-import { VanillaPlayer } from "../../../test/Player.test";
+import { Molang } from "../../src/Lib/Molang/Molang";
+import { VanillaPlayer } from "../Player.test";
 
 describe("molang", () => {
   describe("queries", () => {
