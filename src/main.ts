89fc156ab49b7e8cdd8846deca5c4215bd43783a
DaanV2 <daanverstraten@hotmail.com>
2021-09-20 20:45:17 +0200

    Updating

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 export * as Internal from "./Lib/Internal/include";
-export { DataContent, Molang, MolangFullSet, MolangSet, RegularExpression } from "./Lib/Molang/include";
-export { BehaviorPack, PackType, ProjectData, ResourcePack } from "./Lib/Project/include";
-export { Defined, DefinedUsing, JsonPath, Map, Container, DataSet, Documentation, ProjectContext, Pack, Script, TextDocument, Using } from "./Lib/Types/include";
+export * from "./Lib/Molang/include";
+export * from "./Lib/Project/include";
+
+export * from "./Lib/Types/Defined Using/include";
+export * from "./Lib/Types/DataSet/include";
+export * from "./Lib/Types/Json/JsonPath";
+export * from "./Lib/Types/Map/Map";
+export * from "./Lib/Types/Container/Container";
+
+export * from "./Lib/Types/Documentated/Documentated";
+export * from "./Lib/Types/ProjectContext/ProjectContext";
+export * from "./Lib/Types/Pack/Pack";
+export * from "./Lib/Types/Script/Script";
+export * from "./Lib/Types/TextDocument/TextDocument";
