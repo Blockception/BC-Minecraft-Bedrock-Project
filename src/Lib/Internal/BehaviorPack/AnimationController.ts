b751143a77ad1d95b532cffb14eb542cc20a5fb7
DaanV2 <daanverstraten@hotmail.com>
2021-09-22 18:00:59 +0200

    Fixing processing of ac

--- src/Lib/Internal/BehaviorPack/AnimationController.ts
+++ src/Lib/Internal/BehaviorPack/AnimationController.ts
@@ -43,7 +43,7 @@ export namespace AnimationController {
    * @returns
    */
   export function is(value: any): value is AnimationController {
-    if (typeof value === "object" && typeof value.states === "string") return true;
+    if (typeof value === "object" && typeof value.states === "object") return true;
 
     return false;
   }
