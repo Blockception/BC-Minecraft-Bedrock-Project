3afd9346c512789e3fc3b15d3f177d35117d4bfe
DaanV2 <daanverstraten@hotmail.com>
2021-10-01 11:49:29 +0200

    Added todos for events

--- src/Lib/Project/BehaviorPack/Types/Animation/Animation.ts
+++ src/Lib/Project/BehaviorPack/Types/Animation/Animation.ts
@@ -2,6 +2,8 @@ import { Types } from "bc-minecraft-bedrock-types";
 import { MolangSet } from "bc-minecraft-molang";
 import { MolangCarrier } from '../../../../Types/Carrier/Carrier';
 
+//TODO add events
+
 /** */
 export interface Animation extends Types.BaseObject, MolangCarrier<MolangSet> {
   /** */
--- src/Lib/Project/BehaviorPack/Types/AnimationController/AnimationController.ts
+++ src/Lib/Project/BehaviorPack/Types/AnimationController/AnimationController.ts
@@ -2,6 +2,8 @@ import { Types } from "bc-minecraft-bedrock-types";
 import { DefinedUsing, MolangSet } from "bc-minecraft-molang";
 import { AnimationCarrier } from '../../../../Types/Carrier/Carrier';
 
+//TODO add events
+
 /** */
 export interface AnimationController extends Types.BaseObject, AnimationCarrier<DefinedUsing<string>> {
   /** */
