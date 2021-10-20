7fc1a877447fd839f9a3781199069d482b13a5d2
DaanV2 <daanverstraten@hotmail.com>
2021-10-20 11:51:45 +0200

    Added skinpack filetype

---
+++ src/Lib/Project/SkinPack/Enum/FileType.ts
@@ -0,0 +1,35 @@
+import path = require("path");
+
+/** */
+export enum FileType {
+  /** */
+  manifest,
+  /** */
+  skins,
+  /** */
+  texture,
+  /** */
+  unknown,
+}
+
+/** */
+export namespace FileType {
+  /**Detects resource pack resource, already assumed the path belongs to a resource pack
+   * @param uri the decoded uri, expects slashes to be '/'*/
+  export function detect(uri: string): FileType {
+    //Folders
+    if (uri.endsWith('.png')) return FileType.texture;
+
+    const filename = path.basename(uri);
+
+    switch (filename) {
+      case "manifest.json":
+        return FileType.manifest;
+
+      case "skins.json":
+        return FileType.skins;
+    }
+
+    return FileType.unknown;
+  }
+}
