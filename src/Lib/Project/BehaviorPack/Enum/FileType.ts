7b528cd699722842c19f970237dc07282edaf807
DaanV2 <daanverstraten@hotmail.com>
2022-02-17 09:44:01 +0100

    Updated

--- src/Lib/Project/BehaviorPack/Enum/FileType.ts
+++ src/Lib/Project/BehaviorPack/Enum/FileType.ts
@@ -38,15 +38,16 @@ export namespace FileType {
     //Folders
     if (/[\\\/]animation_controllers[\\\/]/.test(uri)) return FileType.animation_controller;
     if (/[\\\/]animations[\\\/]/.test(uri)) return FileType.animation;
-    if (/[\\\/]blocks[\\\/]/.test(uri)) return FileType.block;
-    if (/[\\\/]entities[\\\/]/.test(uri)) return FileType.entity;
     if (/[\\\/]functions[\\\/]/.test(uri)) return FileType.function;
-    if (/[\\\/]items[\\\/]/.test(uri)) return FileType.item;
     if (/[\\\/]loot_tables[\\\/]/.test(uri)) return FileType.loot_table;
     if (/[\\\/]scripts[\\\/]/.test(uri)) return FileType.script;
     if (/[\\\/]spawn_rules[\\\/]/.test(uri)) return FileType.spawn_rule;
     if (/[\\\/]structures[\\\/]/.test(uri)) return FileType.structure;
     if (/[\\\/]trading[\\\/]/.test(uri)) return FileType.trading;
+    //These can also be subfolders
+    if (/[\\\/]blocks[\\\/]/.test(uri)) return FileType.block;
+    if (/[\\\/]entities[\\\/]/.test(uri)) return FileType.entity;
+    if (/[\\\/]items[\\\/]/.test(uri)) return FileType.item;
 
     //Files
     if (uri.endsWith("manifest.json")) return FileType.manifest;
