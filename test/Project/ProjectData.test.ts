b43eb78c6df41f917cd714c62d56bbab3f5bbcfc
DaanV2 <daanverstraten@hotmail.com>
2023-04-19 20:37:34 +0200

    Fixing tests

--- test/Project/ProjectData.test.ts
+++ test/Project/ProjectData.test.ts
@@ -296,6 +296,7 @@ describe("ProjectData", () => {
       events: [],
       families: [],
       groups: [],
+      properties: [],
     });
     bp.functions.set({ id: "bp.function", location: loc, documentation: doc });
     bp.items.set({
